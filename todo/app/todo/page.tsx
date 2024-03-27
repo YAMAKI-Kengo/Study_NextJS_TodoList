896c2c6d21cd1869ff54a521142c7969896bf010
YAMAKI-Kengo <c1318809@st.kanazawa-it.ac.jp>
2024-03-27 15:10:33 +0900

    UPDATE:ログアウトの位置とボタンデザインの変更

--- todo/app/todo/page.tsx
+++ todo/app/todo/page.tsx
@@ -2,20 +2,8 @@ import TaskTable from "@/components/taskTable";
 
 export default async function Todo() {
   return (
-    <>
-      <div className="pt-10">
-        <form action="/auth/logout" method="post">
-          <button
-            className=" text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-            type="submit"
-          >
-            ログアウト
-          </button>
-        </form>
-      </div>
-      <div className="flex-1 w-full flex flex-col gap-20 items-center pt-24">
-        <TaskTable></TaskTable>
-      </div>
-    </>
+    <div className="flex-1 w-full flex flex-col gap-20 items-center pt-24">
+      <TaskTable></TaskTable>
+    </div>
   );
 }
