9589789cf58101b8d8483a8fdbd709ba1e2f203c
YAMAKI-Kengo <c1318809@st.kanazawa-it.ac.jp>
2024-04-10 07:22:02 +0900

    FIX:コードの修正

--- todo/app/components/NextImage.tsx
+++ todo/app/components/NextImage.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image';
+import Image from "next/image";
 
 export function NextImage() {
-    return (
-        <div className={`fixed top-0 left-0 w-full h-screen z-[-1]`}>
-    <Image src="/mizutama.png"  layout={`fill`} objectFit={`cover`} alt={''} />
+  return (
+    <div className={`fixed top-0 left-0 w-full h-screen z-[-1]`}>
+      <Image src="/mizutama.png" layout={`fill`} objectFit={`cover`} alt={""} />
     </div>
-    )
+  );
 }
--- todo/app/page.tsx
+++ todo/app/page.tsx
@@ -1,16 +1,10 @@
-import { NextImage } from './components/NextImage';
-
-
 export default function Home() {
   return (
-    <>
-
-
-  <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
-        <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
-          Let's
-          <span className="relative whitespace-nowrap text-blue-600"> Todo</span>
-        </h1>
-      </main></>
-  )
+    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pb-16 pt-20 text-center lg:pt-32">
+      <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
+        Let's
+        <span className="relative whitespace-nowrap text-blue-600"> Todo</span>
+      </h1>
+    </main>
+  );
 }
